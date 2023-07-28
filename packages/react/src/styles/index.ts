0375d8ba168f783221ad16d0655135ad9f1615d8
Pedro Requiao <alonsofts@gmail.com>
2023-07-28 10:14:16 -0400

    Adding Tokens into Storybook

--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -1,30 +1,37 @@
-import { colors, fontSizes, fontWeights, fonts, lineHeights, radii, space } from "@ignite-ui/tokens";
-import { createStitches, defaultThemeMap } from "@stitches/react";
+import {
+  colors,
+  fontSizes,
+  fontWeights,
+  fonts,
+  lineHeights,
+  radii,
+  space,
+} from '@ignite-ui/tokens'
+import { createStitches, defaultThemeMap } from '@stitches/react'
 
 export const {
-    styled,
-    css,
-    globalCss,
-    keyframes,
-    getCssText,
-    theme,
-    createTheme,
-    config,
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
 } = createStitches({
+  themeMap: {
+    ...defaultThemeMap,
+    height: 'space',
+    with: 'space',
+  },
 
-    themeMap: {
-        ...defaultThemeMap,
-        height: 'space',
-        with: 'space',
-    },
-
-    theme: {
-        colors: colors,
-        fontSizes: fontSizes,
-        fontWeights: fontWeights,
-        fonts: fonts,
-        lineHeights: lineHeights,
-        radii: radii,
-        space: space,
-    }
+  theme: {
+    colors,
+    fontSizes,
+    fontWeights,
+    fonts,
+    lineHeights,
+    radii,
+    space,
+  },
 })
