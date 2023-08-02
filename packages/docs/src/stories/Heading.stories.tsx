7259e9ce1900d8780c5b879d8e5466bc16add002
Pedro Requiao <alonsofts@gmail.com>
2023-08-02 16:58:14 -0400

    Adding components into Storybook

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,28 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Heading, HeadingProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Heading',
+  component: Heading,
+  tags: ['autodocs'],
+  args: {
+    children: 'Custom Title',
+  },
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+
+export const CustomTag: StoryObj<HeadingProps> = {
+  args: {
+    children: 'H1 Heading',
+    as: 'h1',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'By default, heading will always be `H2`, but we may change it by the prop `as',
+      },
+    },
+  },
+}
---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, nihil! Nulla, saepe quo corporis aliquam ipsam quod dolore rem officiis modi veniam dolores quas, dignissimos tempore alias officia ipsum ad?,',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
