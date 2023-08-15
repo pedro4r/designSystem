fa6e1f2b87bee3d535f91083f618be8ef55f2ad3
Pedro Requiao <alonsofts@gmail.com>
2023-08-15 16:09:16 -0400

    change repo name and pathways

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -19,7 +19,7 @@ const config = {
   },
   viteFinal: (config, {configType}) => {
     if(configType === 'PRODUCTION') {
-      config.base = '/designsystem/'
+      config.base = '/jupiter-ui/'
     }
     return config
   }
